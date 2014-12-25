8148da5c65d1f8ac8101e08962513bf62d8f9f88
Tony Arkles <t.arkles@gmail.com>
2014-12-24 22:12:54 -0600

    initial commit of sample code

---
+++ src/main.c
@@ -0,0 +1,34 @@
+#include "stm32f0xx_conf.h"
+
+void SysTick_Handler(void) {
+  static uint16_t tick = 0;
+  static char val = 0;
+
+  switch (tick++) {
+  	case 100:
+  		tick = 0;
+		if (val) { val = 0; } else { val = 1; }
+  		GPIOA->ODR = (GPIOA->ODR & 0xFFFFFE) | val;
+  		break;
+  }
+}
+
+int main(void)
+{
+	volatile int x = 0;
+
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN; 	// enable the clock to GPIOC
+						//(RM0091 lists this as IOPCEN, not GPIOCEN)
+	__asm("dsb");
+
+	GPIOA->MODER = 0x00000015;
+
+	for(x = 0; x < 100; x++);
+	
+	//SysTick_Config(SystemCoreClock/100);
+
+	while(1) {
+		GPIOA->ODR ^= 0x7;
+	}	
+
+}
