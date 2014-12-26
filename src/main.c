4c24a8485016895873730b6e0cd2eebf5066f29a
Tony Arkles <t.arkles@gmail.com>
2014-12-26 09:51:33 -0600

    WIP getting PWM/Timers working

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include "stm32f0xx_conf.h"
+#include "stm32f0xx_tim.h"
+#include "stm32f0xx_gpio.h"
 
 void SysTick_Handler(void) {
   static uint16_t tick = 0;
@@ -13,19 +15,62 @@ void SysTick_Handler(void) {
   }
 }
 
+void initialize_tim2(void) {
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource0, GPIO_AF_2); // GPIO_AF_2 = TIM2
+
+  TIM_TimeBaseInitTypeDef ti;
+  ti.TIM_Prescaler = 10;
+  ti.TIM_CounterMode = TIM_CounterMode_Up;
+  ti.TIM_Period = 500;
+  ti.TIM_ClockDivision = TIM_CKD_DIV1;
+  ti.TIM_RepetitionCounter = 0;
+  TIM_TimeBaseInit(TIM2, &ti);
+  TIM_Cmd(TIM2, ENABLE);
+
+  TIM_OCInitTypeDef oc = {0,};
+  oc.TIM_OCMode = TIM_OCMode_PWM1;
+  oc.TIM_OutputState = TIM_OutputState_Enable;
+  oc.TIM_OutputNState = 0;
+  oc.TIM_Pulse = 250;
+  oc.TIM_OCPolarity = TIM_OCPolarity_High;
+  oc.TIM_OCNPolarity = 0;
+  oc.TIM_OCIdleState = 0;
+  oc.TIM_OCNIdleState = 0;
+
+  TIM_OC1Init(TIM2, &oc);
+  TIM_OC1PreloadConfig(TIM2, TIM_OCPreload_Enable);
+
+  TIM_OC2Init(TIM2, &oc);
+  TIM_OC2PreloadConfig(TIM2, TIM_OCPreload_Enable);
+
+}
+
 int main(void)
 {
 	volatile int x = 0;
 
-	RCC->AHBENR |= RCC_AHBENR_GPIOAEN; 	// enable the clock to GPIOC
-						//(RM0091 lists this as IOPCEN, not GPIOCEN)
-	__asm("dsb");
+        //RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+        RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+        __asm("dsb");
 
-	GPIOA->MODER = 0x00000015;
+        GPIO_InitTypeDef gp;
+        gp.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
+        gp.GPIO_Mode = GPIO_Mode_AF;
+        gp.GPIO_Speed = GPIO_Speed_50MHz;
+        gp.GPIO_OType = GPIO_OType_PP;
+        gp.GPIO_PuPd = GPIO_PuPd_NOPULL;
+        GPIO_Init(GPIOA, &gp);
 
-	for(x = 0; x < 100; x++);
-	
-	SysTick_Config(SystemCoreClock/100);
+        gp.GPIO_Pin = GPIO_Pin_2;
+        gp.GPIO_Mode = GPIO_Mode_OUT;
+        gp.GPIO_Speed = GPIO_Speed_10MHz;
+        gp.GPIO_OType = GPIO_OType_PP;
+        gp.GPIO_PuPd = GPIO_PuPd_NOPULL;
+        GPIO_Init(GPIOA, &gp);
 
-	while(1);
+        initialize_tim2();
+	while(1) {
+          GPIOA->ODR ^= 0x04;
+        }
 }
