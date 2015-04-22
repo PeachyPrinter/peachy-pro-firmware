949b659a8bec8b39b0c56a0675aaf16dc01aec7a
Tony Arkles <t.arkles@gmail.com>
2015-04-22 15:11:08 -0600

    Stripping the separate interfaces into a single one

--- lib/persea_usb/src/usb_control.c
+++ lib/persea_usb/src/usb_control.c
@@ -66,7 +66,7 @@ static const uint8_t DeviceQualifier[USB_LEN_DEV_QUALIFIER_DESC] =
   0x00,
 };
 
-#define USB_CDC_CONFIG_DESC_SIZ (67)
+#define USB_CDC_CONFIG_DESC_SIZ (39)
 const uint8_t CdcConfig[USB_CDC_CONFIG_DESC_SIZ] =
 {
   /*Configuration Descriptor*/
@@ -74,7 +74,7 @@ const uint8_t CdcConfig[USB_CDC_CONFIG_DESC_SIZ] =
   DESC_CONFIGURATION,      /* bDescriptorType: Configuration */
   USB_CDC_CONFIG_DESC_SIZ,                /* wTotalLength:no of returned bytes */
   0x00,
-  0x02,   /* bNumInterfaces: 2 interface */
+  0x01,   /* bNumInterfaces: 1 interface */
   0x01,   /* bConfigurationValue: Configuration value */
   0x00,   /* iConfiguration: Index of string descriptor describing the configuration */
   0x80,   /* bmAttributes: self powered */
@@ -88,39 +88,12 @@ const uint8_t CdcConfig[USB_CDC_CONFIG_DESC_SIZ] =
   /* Interface descriptor type */
   0x00,   /* bInterfaceNumber: Number of Interface */
   0x00,   /* bAlternateSetting: Alternate setting */
-  0x01,   /* bNumEndpoints: One endpoints used */
-  0x02,   /* bInterfaceClass: Communication Interface Class */
-  0x02,   /* bInterfaceSubClass: Abstract Control Model */
-  0x01,   /* bInterfaceProtocol: Common AT commands */
+  0x03,   /* bNumEndpoints: Three endpoints on one interface */
+  0xff,   /* bInterfaceClass: vendor-specific */
+  0x01,   /* bInterfaceSubClass: Version 1 of the Peachy Commands */
+  0x01,   /* bInterfaceProtocol: Version 1 of the Peachy Commands */
   0x00,   /* iInterface: */
-  
-  /*Header Functional Descriptor*/
-  0x05,   /* bLength: Endpoint Descriptor size */
-  0x24,   /* bDescriptorType: CS_INTERFACE */
-  0x00,   /* bDescriptorSubtype: Header Func Desc */
-  0x10,   /* bcdCDC: spec release number */
-  0x01,
-  
-  /*Call Management Functional Descriptor*/
-  0x05,   /* bFunctionLength */
-  0x24,   /* bDescriptorType: CS_INTERFACE */
-  0x01,   /* bDescriptorSubtype: Call Management Func Desc */
-  0x00,   /* bmCapabilities: D0+D1 */
-  0x01,   /* bDataInterface: 1 */
-  
-  /*ACM Functional Descriptor*/
-  0x04,   /* bFunctionLength */
-  0x24,   /* bDescriptorType: CS_INTERFACE */
-  0x02,   /* bDescriptorSubtype: Abstract Control Management desc */
-  0x02,   /* bmCapabilities */
-  
-  /*Union Functional Descriptor*/
-  0x05,   /* bFunctionLength */
-  0x24,   /* bDescriptorType: CS_INTERFACE */
-  0x06,   /* bDescriptorSubtype: Union func desc */
-  0x00,   /* bMasterInterface: Communication class interface */
-  0x01,   /* bSlaveInterface0: Data Class Interface */
-  
+      
   /*Endpoint 2 Descriptor*/
   0x07,                           /* bLength: Endpoint Descriptor size */
   DESC_ENDPOINT,   /* bDescriptorType: Endpoint */
@@ -129,19 +102,6 @@ const uint8_t CdcConfig[USB_CDC_CONFIG_DESC_SIZ] =
   LOBYTE(8),     /* wMaxPacketSize: */
   HIBYTE(8),
   0xFF,                           /* bInterval: */
-  
-  /*---------------------------------------------------------------------------*/
-  
-  /*Data class interface descriptor*/
-  0x09,   /* bLength: Endpoint Descriptor size */
-  DESC_INTERFACE,  /* bDescriptorType: */
-  0x01,   /* bInterfaceNumber: Number of Interface */
-  0x00,   /* bAlternateSetting: Alternate setting */
-  0x02,   /* bNumEndpoints: Two endpoints used */
-  0x0A,   /* bInterfaceClass: CDC */
-  0x00,   /* bInterfaceSubClass: */
-  0x00,   /* bInterfaceProtocol: */
-  0x00,   /* iInterface: */
   
   /*Endpoint OUT Descriptor*/
   0x07,   /* bLength: Endpoint Descriptor size */
