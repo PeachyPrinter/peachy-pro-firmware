36a7824eaab2808b5bc5d697995a03376aeb604c
Harakan <wgt538@gmail.com>
2015-10-16 13:38:15 -0600

    Initial commit of software reprogram library.

---
+++ inc/reprog.h
@@ -0,0 +1,9 @@
+#ifndef __REPROG_H
+#define __REPROG_H
+
+void wipeFlash(void);
+void unlockFlash(void);
+void lockFlash(void);
+void reboot(void);
+
+#endif
---
+++ src/reprog.c
@@ -0,0 +1,36 @@
+#include "reprog.h"
+#include <usb_cdc.h>
+#include "pb_encode.h"
+
+#include "messages.pb.h"
+
+extern bool DEBUG;
+
+volatile int delay;
+//uint32_t KEY1=0x45670123;
+//uint32_t KEY2=0xCDEF89AB;
+
+void wipeFlash(){
+  unlockFlash();
+  FLASH_EraseAllPages();
+  lockFlash();
+  reboot();
+}
+
+void unlockFlash(){
+	__disable_irq(); //Turn off all interrupts so we can do this cleanly
+	delay=1; //Assign a variable as hacky delay
+	FLASH_Unlock(); //Built in.... I love it
+	__enable_irq();
+}
+
+void lockFlash(){
+	__disable_irq(); //Turn off all interrupts so we can do this quickly
+	delay=1; //Assign a variable as hacky delay
+	FLASH_Lock();
+	__enable_irq();
+}
+
+void reboot(){
+	NVIC_SystemReset();
+}
