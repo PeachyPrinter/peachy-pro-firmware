36a90bf47627366013f2cb35a7fb3b44fe1fa168
Rylans Test Computer <software@peachyprinter.com>
2015-11-19 22:30:35 -0600

    typos

--- Program.sh
+++ Program.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+cd /home/peachy/peachy_files/peachy-pro-firmware
+
+echo 'Git repository at:' $PWD
+echo 
 echo 'Short Programming pins and plug in (usually JP4)'
 read -rsp 'LEDS should be off, press any key to program'
-cd /home/peachy/peachy_files/peachy-pro-firmware
 make
 sudo dfu-util -a 0 --dfuse-address 0x08000000 -D main.bin -v -d 16d0:0af3
 #sudo dfu-util 0.5
