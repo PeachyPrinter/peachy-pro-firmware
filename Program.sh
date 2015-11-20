0d4a998482a3252b340bff5be680fea1e9b0702f
Rylans Test Computer <software@peachyprinter.com>
2015-11-19 23:23:36 -0600

    looped the programming for production

--- Program.sh
+++ Program.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 cd /home/peachy/peachy_files/peachy-pro-firmware
 
+echo
 echo 'Git repository at:' $PWD
-echo 
-echo 'Short Programming pins and plug in (usually JP4)'
-read -rsp 'LEDS should be off, press any key to program'
-make
-sudo dfu-util -a 0 --dfuse-address 0x08000000 -D main.bin -v -d 16d0:0af3
-#sudo dfu-util 0.5
+echo 'Short Programming pins and plug in usb (pins labeled JP4)'
+while true; do
+    echo 
+    echo '1) update git'
+    echo '2) program peachy'
+    read -p "Give option (1|2):" input
+    
+    if [ $input == 1 ]; then
+        git pull;
+    elif [ $input == 2 ]; then
+        make
+        dfu-util -a 0 --dfuse-address 0x08000000 -D main.bin -v -d 16d0:0af3
+    fi
+done
