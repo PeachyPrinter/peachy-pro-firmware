886db136c400f50123a564a638a8e5c81cf1179d
James Townley <software@peachyprinter.com>
2016-01-18 16:37:22 -0600

    Made version.sh work

--- version.sh
+++ version.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
-TAG=`git describe --tags --long`
-BRANCH=`git rev-parse --abbrev-ref HEAD`
+SEMANTIC=`cat symantic.version`
 
-if [ "${BRANCH}" != "master" ]; then
-    echo "${TAG}-${BRANCH}"
-else
-    echo "${TAG}"
+function trim() { echo $1; }
+
+if [ -z $GIT_HOME ]; then
+  if [ -f "/usr/local/bin/git" ]; then
+    export GIT_HOME=/usr/local/bin/git
+  elif [ -f "/usr/bin/git" ]; then
+    export GIT_HOME=/usr/bin/git
+  elif [ -f "/bin/git" ]; then
+    export GIT_HOME=/bin/git
+  else
+    echo "Could not find git."
+    exit 1
+  fi
 fi
 
+export GIT_REV_COUNT_RAW=`$GIT_HOME log --pretty=oneline | wc -l`
+export GIT_REV_COUNT=`trim $GIT_REV_COUNT_RAW`
+export GIT_REV=`$GIT_HOME rev-parse HEAD`
+
+VERSION=$SEMANTIC.$TAG$GIT_REV_COUNT
+
+echo "$VERSION"
