01d6b214b452912822e7e18e0158d394456d979d
Harakan <wgt538@gmail.com>
2016-01-22 14:11:47 -0600

    fixing typos

--- inc/overrides.h
+++ inc/overrides.h
@@ -8,7 +8,7 @@
 #ifndef INC_OVERRIDES_H_
 #define INC_OVERRIDES_H_
 
-#define ADC_KEY_EN 0
-#define INTERLOCK_KEY_EN 0
+#define ADC_KEY_EN 1
+#define INTERLOCK_KEY_EN 1
 
 #endif /* INC_OVERRIDES_H_ */
