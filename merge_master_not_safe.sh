4549eff61642ff38f56d88c8c42927be187fae32
Harakan <wgt538@gmail.com>
2016-01-22 15:39:28 -0600

    fixing typo

--- merge_master_not_safe.sh
+++ merge_master_not_safe.sh
@@ -2,9 +2,12 @@
 echo ----------------------
 echo      MERGING
 echo ----------------------
+
 git checkout not_safe
 git merge master
+git push
 git checkout master
+
 echo ----------------------
 echo    DONE MERGING
 echo ----------------------
