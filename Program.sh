e44b6344c15e864361fff9fc59375737d724c7fa
Harakan <wgt538@gmail.com>
2016-01-22 15:38:52 -0600

    Updated helper scripts for programming

--- Program.sh
+++ Program.sh
@@ -6,12 +6,14 @@ echo 'Is the Git repository at:' $PWD ' ?'
 echo 'if not change path inside script'
 echo
 echo 'Short Programming pins and plug in usb (pins labeled JP4)'
+echo 'CTRL + C to quit'
 while true; do
     echo 
     echo '1) program peachy'
     echo '2) compile` not_safe git'
     echo '3) compile master git'
-    read -p "Give option (1|2|3):" input
+		echo '4) merge master into not_safe'
+    read -p "Give option (1|2|3|4):" input
     
     if [ $input == 1 ]; then
         dfu-util -a 0 --dfuse-address 0x08000000 -D main.bin -v -d 16d0:0af3
@@ -19,9 +21,20 @@ while true; do
         git pull;
 				git checkout not_safe
         make
+				echo 
+				echo ----------------------
     elif [ $input == 3 ]; then
         git pull;
 				git checkout master 
         make
+				echo 
+				echo ----------------------
+    elif [ $input == 4 ]; then
+			echo
+			echo 'Please only do this if you know what you are doing - Will'
+			read -p "Are you sure you want to merge? (y|n)" $merge
+			if [[ $merge == "y" ]]; then
+				./merge_master_not_safe.sh
+			fi
     fi
 done
--- merge_master_not_safe.sh
+++ merge_master_not_safe.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+echo ----------------------
+echo      MERGING
+echo ----------------------
 git checkout not_safe
 git merge master
 git checkout master
+echo ----------------------
+echo    DONE MERGING
+echo ----------------------
